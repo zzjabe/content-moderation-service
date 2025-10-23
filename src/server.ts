93e2bad4fb7d72d8199459d33db8001bec4e25a9
Jianbin Zhang <jianbinzhang@rrc.ca>
2025-10-23 18:36:35 -0500

    Updated server.ts to consolelog the env.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from "./app";
 import { Server } from "http";
+import { envConfig } from "../config/envConfig";
 
 const PORT: string | number = process.env.PORT || 3000;
 
 const server: Server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${envConfig.port}`);
+    console.log(`Environment: ${envConfig.nodeEnv}`);
+    console.log(`Morgan log level: ${envConfig.logLevel}`);
 });
 
 export default server;
